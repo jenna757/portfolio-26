14e1392e210783f97552eb73ba0508583281ae57
jenna757 <web.jinseul@gmail.com>
2026-05-16 01:23:35 +0900

    chore(about): 텍스트 강조

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -1,22 +1,18 @@
 const About = () => {
   return (
-    <section
-      id="about"
-      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-      aria-label="소개"
-    >
+    <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="소개">
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-navy/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-sm font-bold uppercase tracking-widest text-heading lg:sr-only">About</h2>
       </div>
       <div>
-        <p className="mb-4 font-medium text-heading">
-          사용자 경험의 경계를 넓히는 웹 퍼블리셔, 최진슬 입니다.
-        </p>
+        <p className="mb-4 font-medium text-heading">사용자 경험의 경계를 넓히는 웹 퍼블리셔, 최진슬 입니다.</p>
         <p className="mb-4">
           저는 단순한 화면 구현을 넘어, <span className="font-medium text-heading">시멘틱 마크업과 WAI-ARIA</span>
-          {
-            '(aria-label 등 사용하여 스크린 리더와 같은 보조 기기에 보이지 않는 요소를 설명 또는 의미를 명확히 전달하는 접근성 기술)를 기반으로 누구나 소외되지 않는 웹 접근성을 설계합니다.'
-          }
+          {''}
+          (aria-label 등 사용하여 스크린 리더와 같은 보조 기기에 보이지 않는 요소를 설명 또는 의미를 명확히 전달하는
+          접근성 기술)를 기반으로 <span className="font-medium text-heading">누구나 소외되지 않는 웹 접근성</span>을
+          설계합니다.
+          {''}
         </p>
         <p className="mb-4">
           최근에는 사내 <span className="font-medium text-heading">디자인 시스템을 제로베이스에서 구축</span>하여
